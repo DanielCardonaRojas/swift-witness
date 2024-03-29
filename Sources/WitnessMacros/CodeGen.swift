9d01b472cbb5d3c937eab97ce9404b76bf7a5e39
Daniel Cardona Rojas <d.cardona.rojas@gmail.com>
2024-03-28 23:36:10 -0500

    Support variable declarations and associated types

--- Sources/WitnessMacros/CodeGen.swift
+++ Sources/WitnessMacros/CodeGen.swift
@@ -18,7 +18,7 @@ public enum WitnessGenerator {
   public static func processProtocol(protocolDecl: ProtocolDeclSyntax) throws -> [DeclSyntax] {
     let structDecl = StructDeclSyntax(
       name: "\(raw: protocolDecl.name.text)Witness",
-      genericParameterClause: GenericParameterClauseSyntax(parameters: GenericParameterListSyntax(arrayLiteral: GenericParameterSyntax(name: TokenSyntax(stringLiteral: Self.genericLabel)))),
+      genericParameterClause: genericParameterClause(protocolDecl),
       memberBlock: MemberBlockSyntax(
         members: MemberBlockItemListSyntax(itemsBuilder: {
           if let inheritedTypes = protocolDecl.inheritanceClause?.inheritedTypes {
@@ -57,10 +57,8 @@ public enum WitnessGenerator {
           }
 
           for member in protocolDecl.memberBlock.members {
-            if let functionDecl = member.decl.as(FunctionDeclSyntax.self) {
-              MemberBlockItemSyntax(decl: processProtocolRequirement(functionDecl))
-            } else {
-
+            if let member = processProtocolRequirement(member.decl) {
+              member
             }
           }
         })
@@ -71,53 +69,103 @@ public enum WitnessGenerator {
       DeclSyntax(structDecl)
     ]
   }
-
-  static private func processProtocolRequirement(_ functionDecl: FunctionDeclSyntax) -> VariableDeclSyntax {
-    VariableDeclSyntax(
-      bindingSpecifier: .keyword(.let),
-      bindings: PatternBindingListSyntax(
-        itemsBuilder: {
-          PatternBindingListSyntax.Element(
-            pattern: IdentifierPatternSyntax(
-              identifier: functionDecl.name
-            ),
-            typeAnnotation: TypeAnnotationSyntax(
-              type: FunctionTypeSyntax(
-                parameters: TupleTypeElementListSyntax(itemsBuilder: {
-                  // Convert mutating to inout
-                  if functionDecl.modifiers.contains(where: { $0.name.text == TokenSyntax.keyword(.mutating).text}) {
-                    TupleTypeElementSyntax(
-                      type:  AttributedTypeSyntax(
-                        specifier: .keyword(.inout),
-                        baseType: IdentifierTypeSyntax(
-                          name: TokenSyntax(stringLiteral: Self.genericLabel)
-                        )
-                      )
-                    )
-
-                  } else {
-                    TupleTypeElementSyntax(
-                      type: IdentifierTypeSyntax(
-                        name: TokenSyntax(stringLiteral: Self.genericLabel)
-                      )
-                    )
-                  }
-
-                  for parameter in functionDecl.signature.parameterClause.parameters {
-                    if let identifierType = parameter.type.as(IdentifierTypeSyntax.self), identifierType.name.text == "Self" {
-                      TupleTypeElementSyntax(
-                        type: IdentifierTypeSyntax(
-                          name: TokenSyntax(stringLiteral: Self.genericLabel)
-                        )
-                      )
-                    } else {
-                      TupleTypeElementSyntax(type: parameter.type)
-                    }
-                  }
-                }), returnClause: ReturnClauseSyntax(
-                  type: Self.replaceSelf(typeSyntax: functionDecl.signature.returnClause?.type ?? TypeSyntax(stringLiteral: "Void")))))
+  
+  static private func processProtocolRequirement(_ decl: DeclSyntax) -> MemberBlockItemSyntax? {
+    if let functionDecl = decl.as(FunctionDeclSyntax.self) {
+      return MemberBlockItemSyntax(
+        decl: VariableDeclSyntax(
+          bindingSpecifier: .keyword(.let),
+          bindings: PatternBindingListSyntax(
+            itemsBuilder: {
+              PatternBindingListSyntax.Element(
+                pattern: IdentifierPatternSyntax(
+                  identifier: functionDecl.name
+                ),
+                typeAnnotation: TypeAnnotationSyntax(
+                  type: functionRequirementWitnessType(functionDecl))
+              )
+            }
           )
+        )
+      )
+    } else if let variableDecl = decl.as(VariableDeclSyntax.self),
+              let identifier = variableDecl.bindings.first?.pattern.as(IdentifierPatternSyntax.self) {
+      return MemberBlockItemSyntax(
+        decl: VariableDeclSyntax(
+          bindingSpecifier: .keyword(.let),
+          bindings: PatternBindingListSyntax(
+            itemsBuilder: {
+              PatternBindingListSyntax.Element(
+                pattern: identifier,
+                typeAnnotation: TypeAnnotationSyntax(
+                  type: variableRequirementWitnessType(variableDecl))
+              )
+            }
+          )
+        )
+      )
+    }
+
+    return nil
+  }
+
+  
+  /// The type of the property created when de-protocolizing a protocol function requirement
+  static private func variableRequirementWitnessType(_ variableDecl: VariableDeclSyntax) -> FunctionTypeSyntax  {
+
+    return FunctionTypeSyntax(
+      parameters: TupleTypeElementListSyntax(itemsBuilder: {
+       if variableDecl.modifiers.contains(where: { $0.name.text == TokenSyntax.keyword(.static).text}) {
+          //Do not add instance param if method is static
+        } else {
+          // Instance reference
+          selfTupleTypeElement()
         }
+      }), returnClause: ReturnClauseSyntax(
+        type: Self.replaceSelf(typeSyntax: variableDecl.bindings.first?.typeAnnotation?.type ?? TypeSyntax(stringLiteral: "Void"))))
+  }
+
+  /// The type of the property created when de-protocolizing a protocol function  requirement
+  static private func functionRequirementWitnessType(_ functionDecl: FunctionDeclSyntax) -> FunctionTypeSyntax  {
+    FunctionTypeSyntax(
+      parameters: TupleTypeElementListSyntax(itemsBuilder: {
+        // Convert mutating to inout
+        if functionDecl.modifiers.contains(where: { $0.name.text == TokenSyntax.keyword(.mutating).text}) {
+          inoutSelfTupleTypeElement()
+        } else if functionDecl.modifiers.contains(where: { $0.name.text == TokenSyntax.keyword(.static).text}) {
+          //Do not add instance param if method is static
+        } else {
+          // Instance reference
+          selfTupleTypeElement()
+        }
+
+        // Protocol requirement parameters
+        for parameter in functionDecl.signature.parameterClause.parameters {
+          if let identifierType = parameter.type.as(IdentifierTypeSyntax.self), identifierType.name.text == "Self" {
+            selfTupleTypeElement()
+          } else {
+            TupleTypeElementSyntax(type: parameter.type)
+          }
+        }
+      }), returnClause: ReturnClauseSyntax(
+        type: Self.replaceSelf(typeSyntax: functionDecl.signature.returnClause?.type ?? TypeSyntax(stringLiteral: "Void"))))
+  }
+
+  static func inoutSelfTupleTypeElement() -> TupleTypeElementSyntax {
+    TupleTypeElementSyntax(
+      type:  AttributedTypeSyntax(
+        specifier: .keyword(.inout),
+        baseType: IdentifierTypeSyntax(
+          name: TokenSyntax(stringLiteral: Self.genericLabel)
+        )
+      )
+    )
+  }
+
+  static func selfTupleTypeElement() -> TupleTypeElementSyntax {
+    TupleTypeElementSyntax(
+      type: IdentifierTypeSyntax(
+        name: TokenSyntax(stringLiteral: Self.genericLabel)
       )
     )
   }
@@ -131,6 +179,53 @@ public enum WitnessGenerator {
 
     return typeSyntax
   }
+  
+  static func genericParameterClause(_ protocolDecl: ProtocolDeclSyntax) -> GenericParameterClauseSyntax? {
+    let nonPrimary = associatedTypeToGenericParam(protocolDecl, primary: false)
+    let primary = associatedTypeToGenericParam(protocolDecl, primary: true)
+
+    let parameters = GenericParameterListSyntax(itemsBuilder: {
+      GenericParameterSyntax(name: TokenSyntax(stringLiteral: Self.genericLabel))
+
+      for parameter in primary {
+        parameter
+      }
+
+      for parameter in nonPrimary {
+        parameter
+      }
+    })
+
+    return GenericParameterClauseSyntax(parameters: parameters)
+  }
+  
+  static func associatedTypeToGenericParam(_ protocolDecl: ProtocolDeclSyntax, primary: Bool) -> [GenericParameterSyntax] {
+    let primaryAssociatedTypeNames = protocolDecl.primaryAssociatedTypeClause?.primaryAssociatedTypes
+
+    let associatedTypes: [GenericParameterSyntax] = protocolDecl.memberBlock.members
+      .compactMap({ member in
+        guard let associatedType = member.decl.as(AssociatedTypeDeclSyntax.self) else {
+          return nil
+        }
+
+        let isPrimary = primaryAssociatedTypeNames?.contains(where: { $0.name.description == associatedType.name.description }) ?? false
+
+        let inheritedTypes = associatedType.inheritanceClause?.inheritedTypes.compactMap({ $0.type.as(IdentifierTypeSyntax.self) }) ?? []
+        let inheritedType = inheritedTypes.first
+
+        let genericParam = GenericParameterSyntax(name: associatedType.name,
+                                                  colon: inheritedType != nil ? .colonToken() : nil,
+                                                  inheritedType: inheritedType)
+        if primary {
+          return isPrimary ? genericParam : nil
+        } else {
+          return isPrimary ? nil : genericParam
+        }
+      })
+
+    return associatedTypes
+  }
+
 }
 
 extension String {
