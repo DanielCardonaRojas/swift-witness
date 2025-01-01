f17ce9b7c1b741840cfbc28ca26c330bc2d6c2de
Daniel Cardona Rojas <d.cardona.rojas@gmail.com>
2025-01-01 14:12:42 -0500

    Add conformanceInit option
    Generate a special constructor for the witness struct

--- Sources/Witness/Witness.swift
+++ Sources/Witness/Witness.swift
@@ -1,5 +1,6 @@
 public enum WitnessOptions: String {
   case utilities // map, pullback or iso
+  case conformanceInit // a initializer creating a Witness from a conformance to the protocol
 }
 
 @attached(peer, names: suffixed(Witness))
